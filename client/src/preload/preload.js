9e3eee2eccc5148570c587db48457b8d3860c540
Atharva <atharvabutte03@gmail.com>
2026-08-26 21:01:07 +0530

    feat: add electron cryptographic identity bridge

--- client/src/preload/preload.js
+++ client/src/preload/preload.js
@@ -3,7 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   platform: process.platform,
   version: process.versions.electron,
+
+  // Cycle 1 Session IPC
   saveSession: (token) => ipcRenderer.invoke('save-session', token),
   getSession: () => ipcRenderer.invoke('get-session'),
   clearSession: () => ipcRenderer.invoke('clear-session'),
+
+  // Cycle 2 Cryptographic Identity IPC
+  getIdentityStatus: () => ipcRenderer.invoke('get-identity-status'),
+  ensureIdentity: () => ipcRenderer.invoke('ensure-identity'),
 });
